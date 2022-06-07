3049803e7014be6b208ee86c12dc64b7150f7639
adaluong <35322993+adaluong@users.noreply.github.com>
2022-06-07 16:31:14 +1000

    update tut01 files

---
+++ tut01/face.c
@@ -0,0 +1,14 @@
+// Printing a face
+// Ada Luong, May 2022
+
+#include <stdio.h>
+
+int main (void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+
+    return 0;
+}
